cde57e0757aa236858eeb32abbc21a99807bba7d
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-03-20 21:19:15 +0600

    fix the search & fetch the result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Input from './components/Input/Input';
-import Phones from './components/Phones/Phones';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Phones from "./components/Phones/Phones";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Input></Input>
       <Phones></Phones>
       <Footer></Footer>
     </div>
--- src/components/Phones/Phones.js
+++ src/components/Phones/Phones.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import Phone from '../Phone/Phone';
-import './Phones.css'
+import { useEffect, useState } from "react";
+import Phone from "../Phone/Phone";
+import "./Phones.css";
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
     // console.log(phones)
     useEffect(() => {
         fetch(`https://openapi.programming-hero.com/api/phones?search=apple`)
-            .then(res => res.json())
-            .then(data => setPhones(data.data));
-    }, [])
+            .then((res) => res.json())
+            .then((data) => setPhones(data.data));
+    }, []);
+    //   handle input change & add to search field
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${value}`)
+            .then((res) => res.json())
+            .then((data) => setPhones(data.data));
+    };
 
     return (
-        <div className='container'>
-            <p className='text-center mt-5 mb-5 available'>Available {phones.length} {phones[0]?.brand} phones now</p>
-            <div className='phones-container'>
-                {
-                    phones.map(phone => <Phone phone={phone} key={phone.slug}></Phone>)
-                }
+        <div className="container">
+            <div className="input-group mb-3 mx-auto my-5 search-input">
+                <input
+                    onBlur={handleInputChange}
+                    id="search-field"
+                    type="text"
+                    className="form-control"
+                    placeholder="Search your phone..."
+                    aria-label=""
+                    aria-describedby="button-addon2"
+                ></input>
+                <button
+                    onClick={handleInputChange}
+                    id="search-btn"
+                    className="btn btn-outline-secondary"
+                    type="button"
+                >
+                    Search
+                </button>
+            </div>
+            <p className="text-center mt-5 mb-5 available">
+                Available {phones.length} {phones[0]?.brand} phones now
+            </p>
+            <div className="phones-container">
+                {phones.map((phone) => (
+                    <Phone phone={phone} key={phone.slug}></Phone>
+                ))}
             </div>
         </div>
     );
